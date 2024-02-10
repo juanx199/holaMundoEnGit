02fb14b79b98c698e0fe6619acddb6e717199766
juanx199 <jccarvajalcamargo@gmail.com>
2024-02-10 17:35:39 -0500

    mazo con números

--- generador.py
+++ generador.py
@@ -1,5 +1,5 @@
 pintas = ["picas", "treboles", "diamantes", "corazones"]
-valores = ["A", "J", "Q", "K" ]
+valores = ["A", "J", "Q", "K" ] + [str(i) for i in range (2,11)]
 mazo = [(v,p) for v in valores for p in pintas]
 for c in mazo:
     print (c)
